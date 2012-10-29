14c1485e506c06979be9ecb0a0a8b0a848fdf654
Tri Nguyen <nvutri@gmail.com>
2012-10-28 22:26:22 -0500

    Implement class Graph

---
+++ Graph.h
@@ -0,0 +1,41 @@
+#include <vector>
+#include "Node.h"
+#ifndef _GRAPH_H
+#define _GRAPH_H
+
+const int DEFAULT_SIZE = 100;
+
+class Graph {
+private:
+	std::vector< Node > _nodes;
+	;
+public:
+	int size() {
+		return _nodes.size();
+	}
+	Graph();
+	Graph(int num_node);
+	Node &operator[](int node);
+	void print();
+};
+
+Graph::Graph(){
+	// Initialize an empty _node vector
+	for (int i = 0; i < DEFAULT_SIZE; ++i) {
+		_nodes.push_back(Node());
+	}
+}
+
+Graph::Graph(int num_node){
+	// Initialize an empty _node vector
+	for (int i = 0; i < num_node; ++i) {
+		_nodes.push_back(Node());
+	}
+}
+
+//Todo change to CPR
+Node& Graph::operator [] (int node){
+	return _nodes[ node ] ;
+}
+
+#endif
